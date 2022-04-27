80c8dbe38fa30a3862f2012c923ffa645cf65d81
CRX <94463746+crxcrm@users.noreply.github.com>
2022-04-27 16:48:07 +0200

    Update SUM over a database.sql

--- sql/SUM over a database.sql
+++ sql/SUM over a database.sql
@@ -1,4 +1,3 @@
-
 -- Given a database, sum all values stored in all columns of type integer across all the user-created tables. The result of the query must be stored in a column called total of type integer.
 -- Note: it should be possible to execute your code multiple times without errors.
 

70b8377a075b23ff7159b396664749c518e078fd
CRX <94463746+crxcrm@users.noreply.github.com>
2022-04-27 11:43:36 +0200

    Update SQL with Sailor Moon: Thinking about JOINs.sql

--- sql/SQL with Sailor Moon: Thinking about JOINs.sql
+++ sql/SQL with Sailor Moon: Thinking about JOINs.sql
@@ -9,4 +9,4 @@
 
 select senshi_name as sailor_senshi, real_name_jpn as real_name, cats.name as cat, schools.school 
 from sailorsenshi left join cats on sailorsenshi.cat_id = cats.id left join
-schools on sailorsenshi.school_id = schools.id
+schools on sailorsenshi.school_id = schools.id;

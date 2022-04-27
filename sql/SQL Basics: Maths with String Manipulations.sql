b32ec1370a327a996c8a32ae249dc43d16d5f332
CRX <94463746+crxcrm@users.noreply.github.com>
2022-04-27 11:45:37 +0200

    Update SQL Basics: Maths with String Manipulations.sql

--- sql/SQL Basics: Maths with String Manipulations.sql
+++ sql/SQL Basics: Maths with String Manipulations.sql
@@ -4,4 +4,4 @@
 
 -- return a single column named calculation where the value is the bit length of name, added to the number of characters in race.
 
-select (bit_length(name)+LENGTH(race)) as calculation from demographics
+select (bit_length(name)+LENGTH(race)) as calculation from demographics;

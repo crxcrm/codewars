6aa5d24811f6877d8d96babf7464b153e802537d
CRX <94463746+crxcrm@users.noreply.github.com>
2022-04-27 14:36:30 +0200

    Create Training on bit counting.py

---
+++ python/Training on bit counting.py
@@ -0,0 +1,10 @@
+#Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+#Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+def count_bits(n):
+    binary = format(n, 'b')
+    count = 0
+    for i in range(0, len(binary)):
+        if binary[i] == "1":
+            count += 1
+    return count

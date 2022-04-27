78a3f0c8812d6bbe6291bdddd0b3d214e31c6c20
CRX <94463746+crxcrm@users.noreply.github.com>
2022-04-27 16:47:52 +0200

    Create SUM over a database.sql

---
+++ sql/SUM over a database.sql
@@ -0,0 +1,37 @@
+
+-- Given a database, sum all values stored in all columns of type integer across all the user-created tables. The result of the query must be stored in a column called total of type integer.
+-- Note: it should be possible to execute your code multiple times without errors.
+
+CREATE OR REPLACE FUNCTION pg_catalog.calcTotal ()
+
+RETURNS BIGINT AS $$
+
+DECLARE
+
+table_name text;
+col_name text;
+val BIGINT := 0;
+total BIGINT := 0;
+
+BEGIN
+
+FOR table_name, col_name IN SELECT t.tablename, c.column_name
+FROM pg_catalog.pg_tables as t
+join information_schema.columns as c  on c.table_name=t.tablename and c.data_type='integer'
+WHERE schemaname != 'pg_catalog' AND 
+    schemaname != 'information_schema' LOOP
+val := 0;
+EXECUTE 'SELECT sum(' || col_name || ') FROM ' || table_name into val;
+
+IF val IS NOT NULL then
+  total := val + total;
+end if;
+
+END LOOP;
+
+RETURN total;
+END;
+
+$$  LANGUAGE plpgsql;
+
+SELECT pg_catalog.calcTotal() AS total;

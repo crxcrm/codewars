72b1b653e5363f2b2d51b90e664cd56af85055e2
CRX <94463746+crxcrm@users.noreply.github.com>
2022-04-28 17:19:01 +0200

    Create Sum of Digits Digital Root.py

---
+++ Sum of Digits Digital Root.py
@@ -0,0 +1,6 @@
+# Digital root is the recursive sum of all the digits in a number.
+# Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. 
+# The input will be a non-negative integer.
+
+def digital_root(n):
+    return (n - 1) % 9 + 1 if n else 0

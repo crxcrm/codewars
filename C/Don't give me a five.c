ce0105a7065b4a0f9a44be8bd0b5539ddbce7c4d
CRX <94463746+crxcrm@users.noreply.github.com>
2022-05-09 14:41:13 +0200

    Create Don't give me a five.c

---
+++ C/Don't give me a five.c
@@ -0,0 +1,35 @@
+# Don't give me five!
+# In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+  
+int dontGiveMeFive(int start, int end)
+{
+	int cont = 0;
+	int zero, aux, j;
+
+	for(int i=start; i<=end; i++)
+	{
+		zero = 1;
+		aux = 0;
+		if(i<0)
+		{
+			j = -1 * i;
+		}
+		else
+		{
+			j = i;
+		}
+		while(zero < j)
+		{
+			if((j%(zero*10) == 5) || (j/(zero*10) == 5))
+			{
+				aux = 1;
+				break;
+			}
+			zero *= 10;
+		}
+		if(aux != 1)
+		{
+			cont++;
+		}
+	}
+	return cont;

69d95d9479c7e67c3cb8fed8d8a2125ab662631d
CRX <94463746+crxcrm@users.noreply.github.com>
2022-05-12 12:50:47 +0200

    Create Convert a boolean to a string.cpp

---
+++ C++/Convert a boolean to a string.cpp
@@ -0,0 +1,15 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+#include <string>
+
+std::string boolean_to_string(bool b){
+  if(b == false){
+    return("false");
+    }else{
+    return("true");
+    }
+}

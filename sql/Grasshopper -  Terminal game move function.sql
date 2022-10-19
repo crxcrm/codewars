fd874b61a5270a0a95b6ab4cf32234780bb0471c
CRX <94463746+crxcrm@users.noreply.github.com>
2022-10-19 15:00:42 +0200

    Create Grasshopper -  Terminal game move function.sql

---
+++ sql/Grasshopper -  Terminal game move function.sql
@@ -0,0 +1,2 @@
+--# write your statement here: you will be given a table 'moves' with columns 'position' and 'roll'. return a table with a column 'res'. #--
+select position + roll * 2 as res from moves;
